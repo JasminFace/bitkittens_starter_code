3c1081160db481a40374ef4be0bb9333ca436226
Jasmin Saromo <jasminsaromo@gmail.com>
2019-06-20 20:47:42 -0400

    add js to add cat in each box

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  const summon = document.querySelector('.summon-cats');
+
+  summon.addEventListener('click', function() {
+    axios.get('http://bitkittens.herokuapp.com/cats.json')
+      .then(response => {
+        let cats = response.data.cats;
+        let boxes = document.querySelectorAll('.cat-box');
+        
+        boxes.forEach((box, index) => {
+          let image = document.createElement('img');
+          image.src = cats[index].photo;
+          image.alt = "Photo of " + cats[index].name;
+
+          if (box.childElementCount > 0) {
+            box.replaceChild(image, box.firstChild);
+          } else {
+            box.appendChild(image);
+          }
+
+        })
+      
+      })
+
+  })
+
 });
